1bd52ab8d914d9edc5275d1e8bce003ffebba421
Rwhytm <rajuwahyudi842@gmail.com>
2022-08-15 08:02:37 +0700

    deleted linux and web

--- lib/menu.dart
+++ lib/menu.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_database/firebase_database.dart';
 import "package:flutter/material.dart";
 
 class Menu extends StatefulWidget {
@@ -8,12 +9,23 @@ class Menu extends StatefulWidget {
 }
 
 class _MenuState extends State<Menu> {
+  final referencesData = FirebaseDatabase.instance
+      .ref()
+      .child('https://yang-terbaru-default-rtdb.firebaseio.com/')
+      .child('arus listrik');
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
         child: Container(
-          child: Text('Menu'),
+          child: StreamBuilder(
+            stream: referencesData.onValue,
+            builder: (context, AsyncSnapshot snapshot) {
+              if (snapshot.hasData) {
+                return Text(snapshot.data);
+              }
+            },
+          ),
         ),
       ),
     );
